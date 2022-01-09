4f880262ee6bd29f635ce644128c4bb042e59119
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-01-09 03:14:17 +0000

    much

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var req=require,
   server=req("http").Server(),
-  proxy=new(req("./"))({server:server, wss: true, corrosion: [false, {}]});server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html"))));server.listen(80,()=>console.log('80'));
+  proxy=new(req("./"))({server:server, wss: true, corrosion: [false, {}]});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(8080);
