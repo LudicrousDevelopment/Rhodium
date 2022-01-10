5521d0625ea743fb1bb87b59baaa55cfe812e632
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-01-10 18:22:01 +0000

    maybe

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var req=require,
   server=req("http").Server(),
-  proxy=new(req("./"))({server:server, wss: true, corrosion: [false, {}]});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(8080);
+  proxy=new(req("./"))({server:server, wss: true, corrosion: [false, {}], favicon: 'https://discord.com/'});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(8080);
