047ca9d4dab6842e41cbef829253d9048d7ce7b3
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-01-13 22:22:12 +0000

    heroku port support

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var req=require,
   server=req("http").Server(),
-  proxy=new(req("./"))({title:"Rhodium",server:server, wss: true, corrosion: [false, {}], favicon: 'https://discord.com/', userAgent: 'MOZILLA LOL'});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(8080);
+  proxy=new(req("./"))({title:"Rhodium",server:server, wss: true, corrosion: [false, {}], favicon: 'https://discord.com/', /*userAgent: 'MOZILLA LOL'*/});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(process.env.PORT||8080);
