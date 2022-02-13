e3221ac4b1a06d4000594e68c5b05851db91ea5e
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-02-13 02:06:40 +0000

    tests, but is working

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var req=require,
   server=req("http").Server(),
-  proxy=new(req("./"))({title:"Rhodium",server:server, wss: true, corrosion: [false, {}], favicon: 'https://discord.com/', userAgent: undefined});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(process.env.PORT||8080);
+  proxy=new(req("./"))({encode: 'plain',title:"Rhodium",server:server, wss: true, corrosion: [false, {}], favicon: 'https://discord.com/', userAgent: undefined});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(process.env.PORT||8080);
