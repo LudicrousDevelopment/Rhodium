dc2a481253baf0362fe713a1be87af909b6bde99
Ender <78042486+s-tn@users.noreply.github.com>
2024-03-17 18:34:30 -0400

    updates

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var req=require,
-  server=req("http").Server(),
-  proxy=new(req("./"))({encode: 'plain',title:"Rhodium",server:server, wss: true, corrosion: [false, {}], favicon: 'https://discord.com/'});proxy.init();server.on("request",(e,r)=>(e.url.startsWith(proxy.prefix)?proxy.request(e,r):r.end(req("fs").readFileSync("./index.html")))).listen(process.env.PORT||8080);
+var req = require,
+  server = req("http").Server(),
+  proxy = new (req("./"))({
+    encode: "xor",
+    title: "Rhodium",
+    server: server,
+    wss: true,
+    corrosion: [false, {}],
+    favicon: "https://discord.com/"
+  });
+proxy.init();
+server
+  .on("request", (e, r) =>
+    e.url.startsWith(proxy.prefix)
+      ? proxy.request(e, r)
+      : r.end(req("fs").readFileSync("./index.html"))
+  )
+  .listen(process.env.PORT || 3000);
+
